5e963e1d61d438be4db987c2a4931289325ab355
ktochow1 <ktochow1@binghamton.edu>
2019-03-20 23:31:11 +0000

    Automatically backed up by Learn

--- db/migrate/008_add_show_columns.rb
+++ db/migrate/008_add_show_columns.rb
@@ -2,8 +2,8 @@ class AddShowColumns::Migration[5.1]
   
   def change 
     add_column :shows, :day, :string 
-    # add_column :shows, :season, :string 
-    # add_column :shows, :genre, :string
+    add_column :shows, :season, :string 
+    add_column :shows, :genre, :string
   end
 end
   
